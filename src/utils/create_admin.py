6204a834044665bc1d554358eda32de7475995d8
Raphael Golubev <raphael.golubev@gmail.com>
2024-11-06 17:08:24 +0300

    added admin creation helper script

---
+++ src/utils/create_admin.py
@@ -0,0 +1,19 @@
+# create_superuser.py
+import os
+from django.contrib.auth import get_user_model
+
+
+def create_superuser(username, email, password):
+    User = get_user_model()
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+        print(f"Суперпользователь '{username}' успешно создан.")
+    else:
+        print(f"Суперпользователь '{username}' уже существует.")
+
+
+def create_admin():
+    password = os.environ.get("ADMIN_PASSWORD", 'admin')
+    email = os.environ.get("ADMIN_EMAIL", 'admin@example.com')
+    username = os.environ.get("ADMIN_USERNAME", 'admin')
+    create_superuser(username, email, password)
